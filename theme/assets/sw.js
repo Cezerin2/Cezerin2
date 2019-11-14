bdc733433241d8dbf73c5b8d09c4b53324a7ef41
vam <vamshop@gmail.com>
2019-11-14 20:31:19 +0300

    Fixes

--- theme/assets/sw.js
+++ theme/assets/sw.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
-
 importScripts(
-  "/assets/precache-manifest.8fe85deb9f7e5149dd26f665b43bec17.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
 );
+
+importScripts("/assets/precache-manifest.082cde358263644d4628357386f66e76.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
@@ -29,7 +29,23 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(/\/(images|assets|admin-assets)\//, workbox.strategies.cacheFirst(), 'GET');
-workbox.routing.registerRoute(/\/api\//, workbox.strategies.networkOnly(), 'GET');
-workbox.routing.registerRoute(/\/ajax\/payment_form_settings/, workbox.strategies.networkOnly(), 'GET');
-workbox.routing.registerRoute(/\//, workbox.strategies.networkFirst({ networkTimeoutSeconds: 10, plugins: [] }), 'GET');
+workbox.routing.registerRoute(
+  /\/(images|assets|admin-assets)\//,
+  workbox.strategies.cacheFirst(),
+  "GET"
+);
+workbox.routing.registerRoute(
+  /\/api\//,
+  workbox.strategies.networkOnly(),
+  "GET"
+);
+workbox.routing.registerRoute(
+  /\/ajax\/payment_form_settings/,
+  workbox.strategies.networkOnly(),
+  "GET"
+);
+workbox.routing.registerRoute(
+  /\//,
+  workbox.strategies.networkFirst({ networkTimeoutSeconds: 10, plugins: [] }),
+  "GET"
+);
