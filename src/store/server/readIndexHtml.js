6e3406c7a99eebdfcdd795011557ddb740828a6d
vam <vamshop@gmail.com>
2019-11-14 19:50:00 +0300

    TypeError: Cannot read property 'replace' of null issue #18 at cezerin2-store

--- src/store/server/readIndexHtml.js
+++ src/store/server/readIndexHtml.js
@@ -5,11 +5,8 @@ import winston from 'winston';
 const FILE_PATH = path.resolve('theme/assets/index.html');
 export let indexHtml = null;
 
-fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-	if (err) {
-		indexHtml = '';
-		winston.error('Fail to read file', FILE_PATH, err);
-	} else {
-		indexHtml = data;
-	}
-});
+try {
+	indexHtml = fs.readFileSync(FILE_PATH, 'utf8');
+} catch (err) {
+	winston.error('Fail to read file', FILE_PATH, err);
+}
