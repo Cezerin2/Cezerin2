db451c6ee6be24620bb9f3ba779a6b24904110ed
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-14 03:04:39 +0700

    Update (#32)
    * Linted
    
    * Update package.json
    
    1. Linted all files
    2. Updated dependencies
    3. Edited broken Links
    4. Created a lock file

--- config/admin.production.js
+++ config/admin.production.js
@@ -1,6 +1,6 @@
 // config used by dashboard client side only
 module.exports = {
-	// dashboard UI language
-	language: 'en',
-	apiBaseUrl: '/api/v1'
-};
+  // dashboard UI language
+  language: "en",
+  apiBaseUrl: "/api/v1"
+}
--- theme/src/components/checkoutForm/inputField.js
+++ theme/src/components/checkoutForm/inputField.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react"
 
 const InputField = field => (
-	<div className={field.className}>
-		<label htmlFor={field.id}>
-			{field.label}
-			{field.meta.touched && field.meta.error && (
-				<span className="error">{field.meta.error}</span>
-			)}
-		</label>
-		<input
-			{...field.input}
-			placeholder={field.placeholder}
-			disabled={field.disabled}
-			type={field.type}
-			id={field.id}
-			className={field.meta.touched && field.meta.error ? 'invalid' : ''}
-		/>
-	</div>
-);
+  <div className={field.className}>
+    <label htmlFor={field.id}>
+      {field.label}
+      {field.meta.touched && field.meta.error && (
+        <span className="error">{field.meta.error}</span>
+      )}
+    </label>
+    <input
+      {...field.input}
+      placeholder={field.placeholder}
+      disabled={field.disabled}
+      type={field.type}
+      id={field.id}
+      className={field.meta.touched && field.meta.error ? "invalid" : ""}
+    />
+  </div>
+)
 
-export default InputField;
+export default InputField
