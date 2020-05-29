2d26c202724c277f3476d660a3650d224e2cc92e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-30 02:04:44 +0700

    Revert "Updating config (#57)"
    This reverts commit c95e28b2adb608fd699f2f5659878ed7f9586860.

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 // config used by dashboard client side only
-export const language = "en"
-export const apiBaseUrl = "http://localhost:3001/api/v1"
-export const apiWebSocketUrl = "ws://localhost:3001"
-export const developerMode = true
+module.exports = {
+  // dashboard UI language
+  language: "en",
+  apiBaseUrl: "http://localhost:3001/api/v1",
+  apiWebSocketUrl: "ws://localhost:3001",
+  developerMode: true
+}
