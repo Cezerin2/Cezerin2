cc2170efcdf387c78ba45703f6f545bc3caa92ec
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-30 11:36:50 +0700

    Update webpack.config.store.js

--- webpack.config.store.js
+++ webpack.config.store.js
@@ -2,7 +2,7 @@ const path = require("path")
 const webpack = require("webpack")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const CleanWebpackPlugin = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const WorkboxPlugin = require("workbox-webpack-plugin")
 
 module.exports = {
