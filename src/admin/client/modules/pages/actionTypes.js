30efe76e435790d15ba778bbde2249e05601fa7b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-23 10:56:03 +0700

    Move

--- src/admin/client/modules/pages/actionTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAGES_REQUEST = "PAGES_REQUEST";
-exports.PAGES_RECEIVE = "PAGES_RECEIVE";
-exports.PAGE_REQUEST = "PAGE_REQUEST";
-exports.PAGE_RECEIVE = "PAGE_RECEIVE";
-//# sourceMappingURL=actionTypes.js.map
