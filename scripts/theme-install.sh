67e3afae4435762025104c4d2ecd49d9169af379
vam <vamshop@gmail.com>
2020-07-28 15:25:05 +0300

    Theme install script fix

--- scripts/theme-install.sh
+++ scripts/theme-install.sh
@@ -23,6 +23,11 @@ unzip -q "public/content/$fileName" -d "theme"
 
 # 5. build theme
 npm run theme:build
+chmod -R 755 theme/*
 
-# 6. show success message
+# 6. reload apps
+pm2 reload api
+pm2 reload store
+
+# 7. show success message
 echo -e '\e[1;92m'Theme $fileName successfully installed'\e[0m'
