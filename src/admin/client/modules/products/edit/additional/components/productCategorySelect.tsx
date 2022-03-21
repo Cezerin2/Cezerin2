daedf9379949ba6bf13f03ae91702cf2d2028776
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-03-22 00:35:36 +0530

    feat: Product category select to hooks

--- src/admin/client/modules/products/edit/additional/components/productCategorySelect.tsx
+++ src/admin/client/modules/products/edit/additional/components/productCategorySelect.tsx
@@ -3,81 +3,66 @@ import Dialog from "material-ui/Dialog"
 import FlatButton from "material-ui/FlatButton"
 import FontIcon from "material-ui/FontIcon"
 import CategorySelect from "modules/productCategories/select"
-import React, { Fragment } from "react"
+import React, { FC, useState } from "react"
 
-class ProductCategorySelect extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      open: false,
-    }
-  }
+interface props {
+  categories
+  input
+  meta: { touched; error }
+}
 
-  close = () => {
-    this.setState({ open: false })
-  }
+const ProductCategorySelect: FC<props> = props => {
+  const [open, setOpen] = useState(false)
 
-  open = () => {
-    this.setState({ open: true })
-  }
+  const { categories, input } = props
 
-  handleSelect = categoryId => {
-    this.props.input.onChange(categoryId)
-  }
+  const close = () => setOpen(false)
 
-  render() {
-    const {
-      categories,
-      input,
-      meta: { touched, error },
-    } = this.props
-    const { open } = this.state
-    const selectedCategoryId = input.value
-    const category = categories.find(item => item.id === selectedCategoryId)
-    const categoryName = category ? category.name : ""
+  const selectedCategoryId = input.value
+  const category = categories.find(item => item.id === selectedCategoryId)
+  const categoryName = category ? category.name : ""
 
-    const dialogButtons = [
-      <FlatButton
-        label={messages.cancel}
-        onClick={this.close}
-        style={{ marginRight: 10 }}
-      />,
-      <FlatButton
-        label={messages.save}
-        primary
-        keyboardFocused
-        onClick={this.close}
-      />,
-    ]
+  const dialogButtons = [
+    <FlatButton
+      label={messages.cancel}
+      onClick={close}
+      style={{ marginRight: 10 }}
+    />,
+    <FlatButton
+      label={messages.save}
+      primary
+      keyboardFocused
+      onClick={close}
+    />,
+  ]
 
-    return (
-      <Fragment>
-        <Dialog
-          title={messages.category}
-          actions={dialogButtons}
-          modal={false}
-          open={open}
-          onRequestClose={this.close}
-          autoScrollBodyContent
-        >
-          <CategorySelect
-            onSelect={this.handleSelect}
-            selectedId={selectedCategoryId}
-            opened={false}
-          />
-        </Dialog>
-        <FlatButton
-          label={categoryName}
-          onClick={this.open}
-          icon={
-            <FontIcon color="#777" className="material-icons">
-              create
-            </FontIcon>
-          }
+  return (
+    <>
+      <Dialog
+        title={messages.category}
+        actions={dialogButtons}
+        modal={false}
+        open={open}
+        onRequestClose={close}
+        autoScrollBodyContent
+      >
+        <CategorySelect
+          onSelect={input.onChange}
+          selectedId={selectedCategoryId}
+          opened={false}
         />
-      </Fragment>
-    )
-  }
+      </Dialog>
+      <FlatButton
+        label={categoryName}
+        onClick={() => setOpen(true)}
+        icon={
+          <FontIcon color="#777" className="material-icons">
+            create
+          </FontIcon>
+        }
+      />
+    </>
+  )
 }
 
 export default ProductCategorySelect
