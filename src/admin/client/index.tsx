bb71ecc02c6862461a178ba134c2273e90dc59c7
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-03-24 00:48:09 +0530

    feat: Updating redux store to use toolkit

--- src/admin/client/index.tsx
+++ src/admin/client/index.tsx
@@ -1,3 +1,4 @@
+import { configureStore } from "@reduxjs/toolkit"
 import { connectToWebSocket } from "lib/apiWebSocket"
 import * as auth from "lib/auth"
 import settings from "lib/settings"
@@ -5,8 +6,6 @@ import { fetchSettings } from "modules/settings/actions"
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import { applyMiddleware, createStore } from "redux"
-import thunkMiddleware from "redux-thunk"
 import "../../../public/admin-assets/css/flexboxgrid.min.css"
 import "../../../public/admin-assets/css/style.css"
 import App from "./app"
@@ -17,7 +16,7 @@ if (DEVELOPER_MODE === false) {
   auth.validateCurrentToken()
 }
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+const store = configureStore({ reducer: reducers })
 store.dispatch(fetchSettings())
 
 if (window.WebSocket) {
