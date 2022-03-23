8e2eb2ae5cb089b58d171ba7079ee13790273e43
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-03-23 19:12:07 +0530

    chore: Removing unwanted return

--- src/admin/client/modules/products/edit/additional/components/form.tsx
+++ src/admin/client/modules/products/edit/additional/components/form.tsx
@@ -231,114 +231,111 @@ const ProductAdditionalForm = ({
   pristine,
   reset,
   submitting,
-  initialValues,
   settings,
   categories,
-}) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <Paper className="paper-box" zDepth={1}>
-        <div className={style.innerBox}>
-          <div
-            className="row middle-xs"
-            style={{
-              padding: "0 0 15px 0",
-              borderBottom: "1px solid #e0e0e0",
-              marginBottom: 20,
-            }}
-          >
-            <div className="col-xs-12 col-sm-4">{messages.category}</div>
-            <div className="col-xs-12 col-sm-8">
-              <Field
-                name="category_id"
-                component={ProductCategorySelect}
-                categories={categories}
-              />
-            </div>
-          </div>
-
-          <div
-            className="row middle-xs"
-            style={{
-              padding: "0 0 15px 0",
-              borderBottom: "1px solid #e0e0e0",
-              marginBottom: 25,
-            }}
-          >
-            <div className="col-xs-12 col-sm-4">
-              {messages.additionalCategories}
-            </div>
-            <div className="col-xs-12 col-sm-8">
-              <FieldArray
-                name="category_ids"
-                component={ProductCategoryMultiSelect}
-                categories={categories}
-              />
-            </div>
-          </div>
-
-          <div
-            className="row middle-xs"
-            style={{ padding: "0 0 20px 0", borderBottom: "1px solid #e0e0e0" }}
-          >
-            <div className="col-xs-12 col-sm-4">{messages.tags}</div>
-            <div className="col-xs-12 col-sm-8">
-              <Field
-                name="tags"
-                component={TagsField}
-                placeholder={messages.newTag}
-              />
-            </div>
-          </div>
-
-          <div
-            className="row middle-xs"
-            style={{ borderBottom: "1px solid #e0e0e0", marginBottom: 20 }}
-          >
-            <div className="col-xs-12 col-sm-4">{messages.position}</div>
-            <div className="col-xs-12 col-sm-8">
-              <Field
-                name="position"
-                component={TextField}
-                floatingLabelText={messages.position}
-                fullWidth={false}
-                style={{ width: 128 }}
-                type="number"
-              />
-            </div>
-          </div>
-
-          {messages.relatedProducts}
-          <FieldArray
-            name="related_product_ids"
-            component={ProductsArray}
-            settings={settings}
-          />
-        </div>
+}) => (
+  <form onSubmit={handleSubmit}>
+    <Paper className="paper-box" zDepth={1}>
+      <div className={style.innerBox}>
         <div
-          className={
-            "buttons-box " +
-            (pristine ? "buttons-box-pristine" : "buttons-box-show")
-          }
+          className="row middle-xs"
+          style={{
+            padding: "0 0 15px 0",
+            borderBottom: "1px solid #e0e0e0",
+            marginBottom: 20,
+          }}
         >
-          <FlatButton
-            label={messages.cancel}
-            className={style.button}
-            onClick={reset}
-            disabled={pristine || submitting}
-          />
-          <RaisedButton
-            type="submit"
-            label={messages.save}
-            primary
-            className={style.button}
-            disabled={pristine || submitting}
-          />
+          <div className="col-xs-12 col-sm-4">{messages.category}</div>
+          <div className="col-xs-12 col-sm-8">
+            <Field
+              name="category_id"
+              component={ProductCategorySelect}
+              categories={categories}
+            />
+          </div>
         </div>
-      </Paper>
-    </form>
-  )
-}
+
+        <div
+          className="row middle-xs"
+          style={{
+            padding: "0 0 15px 0",
+            borderBottom: "1px solid #e0e0e0",
+            marginBottom: 25,
+          }}
+        >
+          <div className="col-xs-12 col-sm-4">
+            {messages.additionalCategories}
+          </div>
+          <div className="col-xs-12 col-sm-8">
+            <FieldArray
+              name="category_ids"
+              component={ProductCategoryMultiSelect}
+              categories={categories}
+            />
+          </div>
+        </div>
+
+        <div
+          className="row middle-xs"
+          style={{ padding: "0 0 20px 0", borderBottom: "1px solid #e0e0e0" }}
+        >
+          <div className="col-xs-12 col-sm-4">{messages.tags}</div>
+          <div className="col-xs-12 col-sm-8">
+            <Field
+              name="tags"
+              component={TagsField}
+              placeholder={messages.newTag}
+            />
+          </div>
+        </div>
+
+        <div
+          className="row middle-xs"
+          style={{ borderBottom: "1px solid #e0e0e0", marginBottom: 20 }}
+        >
+          <div className="col-xs-12 col-sm-4">{messages.position}</div>
+          <div className="col-xs-12 col-sm-8">
+            <Field
+              name="position"
+              component={TextField}
+              floatingLabelText={messages.position}
+              fullWidth={false}
+              style={{ width: 128 }}
+              type="number"
+            />
+          </div>
+        </div>
+
+        {messages.relatedProducts}
+        <FieldArray
+          name="related_product_ids"
+          component={ProductsArray}
+          settings={settings}
+        />
+      </div>
+      <div
+        className={
+          "buttons-box " +
+          (pristine ? "buttons-box-pristine" : "buttons-box-show")
+        }
+      >
+        <FlatButton
+          label={messages.cancel}
+          className={style.button}
+          onClick={reset}
+          disabled={pristine || submitting}
+        />
+        <RaisedButton
+          type="submit"
+          label={messages.save}
+          primary
+          className={style.button}
+          disabled={pristine || submitting}
+        />
+      </div>
+    </Paper>
+  </form>
+)
 
 export default reduxForm({
   form: "ProductAdditionalForm",
