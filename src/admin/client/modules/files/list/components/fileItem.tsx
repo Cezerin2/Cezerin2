226acd411f9f7bd2bc28600c76de190ef7b86598
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-03-29 14:05:49 +0530

    feat(admin): To react hooks

--- src/admin/client/modules/files/list/components/fileItem.tsx
+++ src/admin/client/modules/files/list/components/fileItem.tsx
@@ -6,7 +6,7 @@ import IconMenu from "material-ui/IconMenu"
 import MenuItem from "material-ui/MenuItem"
 import DeleteConfirmation from "modules/shared/deleteConfirmation"
 import moment from "moment"
-import React from "react"
+import React, { FC, useState } from "react"
 import style from "./style.css"
 
 const iconButtonElement = (
@@ -17,63 +17,55 @@ const iconButtonElement = (
   </IconButton>
 )
 
-class FileItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openDelete: false,
-    }
-  }
+interface Props {
+  file
+  settings
+  onDelete
+}
 
-  showDelete = () => {
-    this.setState({ openDelete: true })
-  }
+const FileItem: FC<Props> = props => {
+  const [openDelete, setOpenDelete] = useState(false)
 
-  hideDelete = () => {
-    this.setState({ openDelete: false })
-  }
+  const { file, settings, onDelete } = props
 
-  handleDelete = () => {
-    const fileName = this.props.file.file
-    this.props.onDelete(fileName)
-    this.hideDelete()
-  }
-
-  render() {
-    const { file, settings } = this.props
+  const handleDelete = () => {
     const fileName = file.file
-    const fileUrl = `${settings.domain}/${file.file}`
-    const modifiedDate = moment(file.modified)
-    const modifiedDateFormated = modifiedDate.format(`${settings.date_format}`)
-    const fileSizeFormated = helper.formatFileSize(file.size)
-
-    return (
-      <div className={style.item + " row row--no-gutter middle-xs"}>
-        <div className={style.name + " col-xs-5"}>
-          <a href={fileUrl} target="_blank" rel="noopener">
-            {file.file}
-          </a>
-        </div>
-        <div className={style.date + " col-xs-3"}>{modifiedDateFormated}</div>
-        <div className={style.size + " col-xs-2"}>{fileSizeFormated}</div>
-        <div className={style.more + " col-xs-2"}>
-          <IconMenu iconButtonElement={iconButtonElement}>
-            <MenuItem onClick={this.showDelete}>
-              {messages.actions_delete}
-            </MenuItem>
-          </IconMenu>
-          <DeleteConfirmation
-            open={this.state.openDelete}
-            isSingle
-            itemsCount={1}
-            itemName={fileName}
-            onCancel={this.hideDelete}
-            onDelete={this.handleDelete}
-          />
-        </div>
-      </div>
-    )
+    onDelete(fileName)
+    setOpenDelete(false)
   }
+
+  const fileName = file.file
+  const fileUrl = `${settings.domain}/${file.file}`
+  const modifiedDate = moment(file.modified)
+  const modifiedDateFormated = modifiedDate.format(`${settings.date_format}`)
+  const fileSizeFormated = helper.formatFileSize(file.size)
+
+  return (
+    <div className={`${style.item} row row--no-gutter middle-xs`}>
+      <div className={`${style.name} col-xs-5`}>
+        <a href={fileUrl} target="_blank" rel="noopener noreferrer">
+          {file.file}
+        </a>
+      </div>
+      <div className={`${style.date} col-xs-3`}>{modifiedDateFormated}</div>
+      <div className={`${style.size} col-xs-2`}>{fileSizeFormated}</div>
+      <div className={`${style.more} col-xs-2`}>
+        <IconMenu iconButtonElement={iconButtonElement}>
+          <MenuItem onClick={() => setOpenDelete(true)}>
+            {messages.actions_delete}
+          </MenuItem>
+        </IconMenu>
+        <DeleteConfirmation
+          open={openDelete}
+          isSingle
+          itemsCount={1}
+          itemName={fileName}
+          onCancel={() => setOpenDelete(false)}
+          onDelete={handleDelete}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default FileItem
--- src/admin/client/modules/files/list/components/form.tsx
+++ src/admin/client/modules/files/list/components/form.tsx
@@ -1,45 +1,44 @@
 import messages from "lib/text"
 import Paper from "material-ui/Paper"
-import React, { Fragment } from "react"
+import React, { FC, useEffect } from "react"
 import FileItem from "./fileItem"
 import FileUploader from "./fileUploader"
 import style from "./style.css"
 
-class FileList extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+interface Props {
+  files
+  settings
+  onDelete
+  onUpload
+  uploading: boolean
+  onLoad: () => void
+}
 
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const FileList: FC<Props> = props => {
+  const { files, settings, onDelete, onUpload, uploading, onLoad } = props
 
-  render() {
-    const { files, settings, onDelete, onUpload, uploading } = this.props
-    let listItems = files.map((file, index) => (
-      <FileItem
-        key={index}
-        file={file}
-        settings={settings}
-        onDelete={onDelete}
-      />
-    ))
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-    return (
-      <Fragment>
-        <div className={style.head + " row row--no-gutter"}>
-          <div className="col-xs-5">{messages.fileName}</div>
-          <div className="col-xs-3">{messages.fileModified}</div>
-          <div className="col-xs-2">{messages.fileSize}</div>
-          <div className="col-xs-2" />
-        </div>
-        <Paper className="paper-box" zDepth={1}>
-          {listItems}
-        </Paper>
-        <FileUploader onUpload={onUpload} uploading={uploading} />
-      </Fragment>
-    )
-  }
+  const listItems = files.map((file, index) => (
+    <FileItem key={index} file={file} settings={settings} onDelete={onDelete} />
+  ))
+
+  return (
+    <>
+      <div className={`${style.head} row row--no-gutter`}>
+        <div className="col-xs-5">{messages.fileName}</div>
+        <div className="col-xs-3">{messages.fileModified}</div>
+        <div className="col-xs-2">{messages.fileSize}</div>
+        <div className="col-xs-2" />
+      </div>
+      <Paper className="paper-box" zDepth={1}>
+        {listItems}
+      </Paper>
+      <FileUploader onUpload={onUpload} uploading={uploading} />
+    </>
+  )
 }
 
 export default FileList
