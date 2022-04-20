34910d76e1a55a4729de2e3560653dc7b4205492
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-04-21 00:36:07 +0530

    chore: Refactoring admin

--- src/admin/client/index.tsx
+++ src/admin/client/index.tsx
@@ -9,18 +9,15 @@ import { Provider } from "react-redux"
 import "../../../public/admin-assets/css/flexboxgrid.min.css"
 import "../../../public/admin-assets/css/style.css"
 import App from "./app"
-import reducers from "./rootReducer"
+import reducer from "./rootReducer"
 
 if (!settings.developerMode) auth.validateCurrentToken()
 
-const store = configureStore({ reducer: reducers })
+const store = configureStore({ reducer })
 store.dispatch(fetchSettings())
 
-if (window.WebSocket) {
-  connectToWebSocket(store)
-} else {
-  console.log("WebSocket is not supported by your browser.")
-}
+if (window.WebSocket) connectToWebSocket(store)
+else console.log("WebSocket is not supported by your browser.")
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
