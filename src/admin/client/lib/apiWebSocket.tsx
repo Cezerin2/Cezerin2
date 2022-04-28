2535f6d65c3cdd1199656e7f4171e28f2bf63e39
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-04-29 03:18:30 +0530

    feat(admin): Fixing imports for redux slices

--- src/admin/client/lib/apiWebSocket.tsx
+++ src/admin/client/lib/apiWebSocket.tsx
@@ -1,7 +1,7 @@
 import settings from "lib/settings"
 import messages from "lib/text"
-import { fetchOrders } from "modules/orders/actions"
-import { installReceive } from "modules/settings/actions"
+import { fetchOrders } from "modules/orders/ordersSlice"
+import { installReceive } from "modules/settings/settingsSlice"
 
 const autoReconnectInterval = 1000 // 1 seconds
 const orderCreated = "order.created"
