b27e276407a1807b5747080b5654255d008e0c27
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-05-08 04:56:07 +0530

    feat(admin): To react hooks

--- src/admin/client/modules/customers/edit/components/details.tsx
+++ src/admin/client/modules/customers/edit/components/details.tsx
@@ -1,57 +1,63 @@
-import React from "react"
+import React, { FC, useEffect } from "react"
 import CustomerAddresses from "./addresses"
 import CustomerOrders from "./orders"
 import CustomerSummary from "./summary"
 
-class CustomerDetails extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+interface Props {
+  fetchData
+  clearData
+  customer
+  settings
+  onCustomerSummaryUpdate
+  onUpdateAddress
+  onDeleteAddress
+  onSetDefaultBillingAddress
+  onSetDefaultShippingAddress
+}
 
-  componentDidMount() {
-    this.props.fetchData()
-  }
+const CustomerDetails: FC<Props> = props => {
+  const {
+    fetchData,
+    clearData,
+    customer,
+    settings,
+    onCustomerSummaryUpdate,
+    onUpdateAddress,
+    onDeleteAddress,
+    onSetDefaultBillingAddress,
+    onSetDefaultShippingAddress,
+  } = props
 
-  componentWillUnmount() {
-    this.props.clearData()
-  }
+  useEffect(() => {
+    fetchData()
+    return () => clearData()
+  }, [])
 
-  render() {
-    const {
-      customer,
-      settings,
-      onCustomerSummaryUpdate,
-      onUpdateAddress,
-      onDeleteAddress,
-      onSetDefaultBillingAddress,
-      onSetDefaultShippingAddress,
-    } = this.props
-    if (!customer) return <br />
+  if (!customer) return <br />
 
-    return (
-      <div className="row row--no-gutter col-full-height">
-        <div className="col-xs-12 col-sm-5 col-md-4 col--no-gutter scroll col-full-height">
-          <CustomerSummary
-            customer={customer}
-            settings={settings}
-            onCustomerSummaryUpdate={onCustomerSummaryUpdate}
-          />
+  return (
+    <div className="row row--no-gutter col-full-height">
+      <div className="col-xs-12 col-sm-5 col-md-4 col--no-gutter scroll col-full-height">
+        <CustomerSummary
+          customer={customer}
+          settings={settings}
+          onCustomerSummaryUpdate={onCustomerSummaryUpdate}
+        />
 
-          <CustomerAddresses
-            customer={customer}
-            settings={settings}
-            onUpdateAddress={onUpdateAddress}
-            onDeleteAddress={onDeleteAddress}
-            onSetDefaultBillingAddress={onSetDefaultBillingAddress}
-            onSetDefaultShippingAddress={onSetDefaultShippingAddress}
-          />
-        </div>
-        <div className="col-xs-12 col-sm-7 col-md-8 col--no-gutter scroll col-full-height">
-          <CustomerOrders customerId={customer.id} settings={settings} />
-        </div>
+        <CustomerAddresses
+          customer={customer}
+          settings={settings}
+          onUpdateAddress={onUpdateAddress}
+          onDeleteAddress={onDeleteAddress}
+          onSetDefaultBillingAddress={onSetDefaultBillingAddress}
+          onSetDefaultShippingAddress={onSetDefaultShippingAddress}
+        />
       </div>
-    )
-  }
+      <div className="col-xs-12 col-sm-7 col-md-8 col--no-gutter scroll col-full-height">
+        <CustomerOrders customerId={customer.id} settings={settings} />
+      </div>
+    </div>
+  )
 }
 
 export default CustomerDetails
