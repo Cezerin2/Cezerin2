a2cec56493e89307c8d87e73b2de74e886dfe2a7
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-08-10 11:06:11 +0530

    fix(admin): Fixing menu in apps

--- src/admin/client/modules/apps/head/components/buttons.tsx
+++ src/admin/client/modules/apps/head/components/buttons.tsx
@@ -1,28 +1,42 @@
+import { MoreVert } from "@mui/icons-material"
+import { IconButton, Menu, MenuItem } from "@mui/material"
 import messages from "lib/text"
-import FontIcon from "material-ui/FontIcon"
-import IconButton from "material-ui/IconButton"
-import IconMenu from "material-ui/IconMenu"
-import MenuItem from "material-ui/MenuItem"
-import React from "react"
+import React, { FC, useState } from "react"
 import { Link } from "react-router-dom"
 
-const WebStoreMenu = () => (
-  <IconMenu
-    iconButtonElement={
-      <IconButton touch>
-        <FontIcon color="#fff" className="material-icons">
-          more_vert
-        </FontIcon>
+const WebStoreMenu: FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const open = Boolean(anchorEl)
+  const handleClose = () => setAnchorEl(null)
+
+  return (
+    <>
+      <IconButton
+        aria-label="more"
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={event => setAnchorEl(event.currentTarget)}
+      >
+        <MoreVert htmlColor="#fff" />
       </IconButton>
-    }
-    targetOrigin={{ horizontal: "right", vertical: "top" }}
-    anchorOrigin={{ horizontal: "right", vertical: "top" }}
-  >
-    <MenuItem
-      containerElement={<Link to="/admin/apps/account" />}
-      primaryText={messages.account}
-    />
-  </IconMenu>
-)
+      <Menu
+        MenuListProps={{
+          "aria-labelledby": "long-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "top" }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to="/admin/apps/account">{messages.account}</Link>
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
 
 export default WebStoreMenu
