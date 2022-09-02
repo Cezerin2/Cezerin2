7021d1c68b9efd0e13c155c685145d9423d227f3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-09-02 15:28:40 +0530

    fix: Updating server middleware

--- src/api/server/index.ts
+++ src/api/server/index.ts
@@ -21,6 +21,21 @@ app.keys = settings.cookieSecretKey
 app.maxIpsCount = 1
 
 app
+  // logger
+  .use(async (ctx, next) => {
+    await next()
+    const rt = ctx.response.get("X-Response-Time")
+    winston.info(`${ctx.method} ${ctx.url} - ${rt}`)
+  })
+
+  // x-response-time
+  .use(async (ctx, next) => {
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    ctx.set("X-Response-Time", `${ms}ms`)
+  })
+
   .use(
     cors({
       origin: security.getAccessControlAllowOrigin(),
@@ -35,21 +50,6 @@ app
   .use(compress({ threshold: 0 })) // compressor
   .use(router.routes()) // router
   .use(router.allowedMethods()) // router
-
-// logger
-app.use(async (ctx, next) => {
-  await next()
-  const rt = ctx.response.get("X-Response-Time")
-  winston.info(`${ctx.method} ${ctx.url} - ${rt}`)
-})
-
-// x-response-time
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.set("X-Response-Time", `${ms}ms`)
-})
 
 router
   .use("/ajax", ajaxRouter.routes(), ajaxRouter.allowedMethods())
--- src/store/server/index.ts
+++ src/store/server/index.ts
@@ -26,6 +26,21 @@ app.keys = settings.cookieSecretKey
 app.maxIpsCount = 1
 
 app
+  // logger
+  .use(async (ctx, next) => {
+    await next()
+    const rt = ctx.response.get("X-Response-Time")
+    winston.info(`${ctx.method} ${ctx.url} - ${rt}`)
+  })
+
+  // x-response-time
+  .use(async (ctx, next) => {
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    ctx.set("X-Response-Time", `${ms}ms`)
+  })
+
   .use(cors()) // cors
   .use(helmet({ contentSecurityPolicy: false })) // helmet
   .use(koaBody()) // body parser
@@ -35,21 +50,6 @@ app
   .use(mount("/admin-assets", serve("public/admin-assets", staticOptions)))
   .use(router.routes()) // router
   .use(router.allowedMethods()) // router
-
-// logger
-app.use(async (ctx, next) => {
-  await next()
-  const rt = ctx.response.get("X-Response-Time")
-  winston.info(`${ctx.method} ${ctx.url} - ${rt}`)
-})
-
-// x-response-time
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.set("X-Response-Time", `${ms}ms`)
-})
 
 router
   .get("/", pageRendering)
