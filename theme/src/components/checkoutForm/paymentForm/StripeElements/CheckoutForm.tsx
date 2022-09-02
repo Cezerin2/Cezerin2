3d4aed338519ed4b3cfc7773bb0fbbf05c2c919f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-09-02 14:00:17 +0530

    feat(theme): Moving to @stripe/react-stripe-js

--- theme/src/components/checkoutForm/paymentForm/StripeElements/CheckoutForm.tsx
+++ theme/src/components/checkoutForm/paymentForm/StripeElements/CheckoutForm.tsx
@@ -1,24 +1,41 @@
-import React, { FC, useState } from "react"
-import { injectStripe } from "react-stripe-elements"
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import React, { FC, FormEventHandler, useState } from "react"
 import CardSection from "./CardSection"
 
 interface Props {
   formSettings
   onCreateToken
-  stripe
 }
 
 const CheckoutForm: FC<Props> = props => {
   const [inProgress, setInProgress] = useState(false)
 
-  const { formSettings, onCreateToken, stripe } = props
+  // Get a reference to Stripe or Elements using hooks.
+  const stripe = useStripe()
+  const elements = useElements()
 
-  const submit = async ev => {
+  const { formSettings, onCreateToken } = props
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async event => {
+    event.preventDefault()
     setInProgress(true)
 
-    const { token } = await stripe.createToken({
+    // Use elements.getElement to get a reference to the mounted Element.
+    const cardElement = elements.getElement(CardElement)
+
+    // Pass the Element directly to other Stripe.js methods:
+    // e.g. createToken - https://stripe.com/docs/js/tokens_sources/create_token?type=cardElement
+    const { token } = await stripe.createToken(cardElement, {
       name: formSettings.email,
     })
+
+    // TODO: confirmCardPayment - https://stripe.com/docs/js/payment_intents/confirm_card_payment
+    // stripe.confirmCardPayment(paymentIntentClientSecret, {
+    //   payment_method: {
+    //     card: cardElement,
+    //   },
+    // })
+
     if (token && token !== "undefined") {
       onCreateToken(token.id)
     } else {
@@ -27,11 +44,10 @@ const CheckoutForm: FC<Props> = props => {
   }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <CardSection title="Credit Card details" />
       <div className="checkout-button-wrap">
         <button
-          onClick={submit}
           disabled={inProgress}
           className={`checkout-button button is-primary${
             inProgress ? " is-loading" : ""
@@ -40,8 +56,8 @@ const CheckoutForm: FC<Props> = props => {
           Confirm order
         </button>
       </div>
-    </div>
+    </form>
   )
 }
 
-export default injectStripe(CheckoutForm)
+export default CheckoutForm
