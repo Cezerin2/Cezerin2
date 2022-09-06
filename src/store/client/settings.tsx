ebbf6cae4146942cf6d87a43d49b85c52c80e806
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-09-06 15:20:08 +0530

    fix(store): Fixing import

--- src/store/client/settings.tsx
+++ src/store/client/settings.tsx
@@ -1,8 +1,3 @@
-import { readFileSync } from "fs-extra"
-import YAML from "yaml"
-import { Store } from "../../../config/types/store"
-
-const file = readFileSync("./config/store.yml", "utf8")
-const settings = YAML.parse(file) as Store
+import settings from "../../../config/store.yml"
 
 export default settings
