8a877d2b8f342bb33165361f47bc11ba874c894b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-09-06 18:50:28 +0530

    fix(store): Fixing settings import (#719)

--- src/store/client/settings.tsx
+++ src/store/client/settings.tsx
@@ -1,3 +1,17 @@
-import settings from "../../../config/store.yml"
+import { readFileSync } from "fs-extra"
+import YAML from "yaml"
+import { Store } from "../../../config/types/store"
+
+let settings: Store
+;(async () => {
+  if (typeof window === "undefined") {
+    const file = readFileSync("./config/store.yml", "utf8")
+    settings = YAML.parse(file) as Store
+    return
+  }
+
+  const config = require("../../../config/store.yml")
+  settings = config.default
+})()
 
 export default settings
