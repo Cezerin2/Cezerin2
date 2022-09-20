2f6ddf67363662f0fb6114252fc04a5adb804310
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-09-20 17:32:15 +0530

    fix: Updating droplet.sh

--- scripts/droplet.sh
+++ scripts/droplet.sh
@@ -30,10 +30,14 @@ yarn global add pm2
 echo "................................"
 
 echo "Installing mongodb and nginx"
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
 sudo apt install mongodb-org nginx -y
 echo "................................"
 
 echo "Cloning Cezerin"
+mkdir /var/www
 cd /var/www
 git clone https://github.com/Cezerin2/Cezerin2.git
 echo "................................"
