b74bdd56cc8eb4c95153765f75ac371896b827ec
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-12-25 13:16:06 +0530

    feat: Updating droplet.sh

--- scripts/droplet.sh
+++ scripts/droplet.sh
@@ -8,11 +8,7 @@ sudo apt upgrade -y
 echo "................................"
 
 echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 echo "................................"
 
 echo "Installing node and enabling yarn"
