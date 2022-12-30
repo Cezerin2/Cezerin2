cb96f251acdd3aa747f6ce725374c3d18dd32925
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2022-12-30 09:26:49 +0530

    fix(theme): Fixing form input

--- theme/src/components/checkoutForm/inputField.tsx
+++ theme/src/components/checkoutForm/inputField.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { FC } from "react"
+import { Field, Form } from "react-final-form"
 
 const InputField = field => (
   <div className={field.className}>
@@ -17,5 +18,52 @@ const InputField = field => (
     />
   </div>
 )
+
+interface SingleInputProps {
+  key?: number
+  name: string
+  id?: string
+  component: "input" | ((field: any) => JSX.Element)
+  type: "text" | "radio"
+  label?: string
+  value?: string | boolean
+  validate?: (value: string) => Record<string, string>
+  onClick?: (value: string) => void
+  className?: string
+}
+
+export const SingleInput: FC<SingleInputProps> = props => {
+  const {
+    className,
+    component,
+    id,
+    key,
+    label,
+    name,
+    onClick,
+    type,
+    validate,
+    value,
+  } = props
+
+  return (
+    <Form onSubmit={() => {}}>
+      {() => (
+        <Field
+          name={name}
+          component={component}
+          key={key}
+          type={type}
+          validate={validate}
+          value={value?.toString()}
+          className={className}
+          id={id}
+          label={label}
+          onClick={onClick}
+        />
+      )}
+    </Form>
+  )
+}
 
 export default InputField
