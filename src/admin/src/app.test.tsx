95d84bd5885293901dd0d1000c378a0c7b7245a6
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2023-05-11 20:12:16 -0700

    feat(admin): Admin to workspaces

---
+++ src/admin/src/app.test.tsx
@@ -0,0 +1,8 @@
+// import React from "react"
+// import { render, screen } from "@testing-library/react"
+
+test("renders learn react link", () => {
+  // render(<App />)
+  // const linkElement = screen.getByText(/learn react/i)
+  // expect(linkElement).toBeInTheDocument()
+})
