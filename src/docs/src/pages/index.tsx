6a9ffa20a32bf5a8128af72d402aa0eea9a5f49f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2023-05-22 08:28:22 -0700

    feat(docs): Updating homepage (#1732)
    * feat(docs): Adding prettier
    
    * feat(docs): Updating homepage

--- src/docs/src/pages/index.tsx
+++ src/docs/src/pages/index.tsx
@@ -1,43 +1,152 @@
 import Link from "@docusaurus/Link"
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
-import HomepageFeatures from "@site/src/components/pages/home"
 import Layout from "@theme/Layout"
-import clsx from "clsx"
 import React from "react"
 
-import styles from "./index.module.css"
+const links = [
+  { title: "GitHub", link: "https://github.com/cezerin2" },
+  { title: "Plusha Theme", link: "https://plusha.cezerin.net" },
+  { title: "Default Theme", link: "https://cezerin.net" },
+  { title: "Dashboard", link: "https://cezerin.net/admin" },
+  { title: "Telegram Chat", link: "https://t.me/cezerin" },
+  { title: "Docs", link: "https://cezerin.org/docs" },
+  { title: "FAQ", link: "https://cezerin.org/docs/#/faq" },
+  { title: "HowTos", link: "https://cezerin.org/docs/#/howtos" },
+  { title: "Facebook", link: "https://facebook.com/cezerin" },
+  { title: "Twitter", link: "https://twitter.com/cezerin2" },
+]
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+const LinkElement = (): JSX.Element => (
+  <div className="grid items-center gap-2 text-center text-lg font-semibold sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-8 2xl:grid-cols-10">
+    {links.map(({ title, link }) => (
+      <Link
+        to={link}
+        className="rounded-md bg-slate-500 p-2 text-white dark:bg-white dark:text-black"
+      >
+        {title}
+      </Link>
+    ))}
+  </div>
+)
+
+const Home = (): JSX.Element => (
+  <Layout
+    title="Cezerin - ReactJS eCommerce platform. React Shopping Cart."
+    description="Cezerin is open-source ecommerce platform. React Shopping Cart. Allows creating a Progressive Web Apps built with React and Node.js. API-first CMS."
+  >
+    <div className="mx-10 my-6 grid gap-12 md:mx-44 md:my-24">
+      <header className="text-center">
+        <h1>Ecommerce Progressive Web Apps</h1>
+        <p>
+          Cezerin is React and Node.js based eCommerce platform. React Shopping
+          Cart. Single-page application. Open-Source.
+        </p>
+        <LinkElement />
+      </header>
+
+      <main className="grid gap-8">
+        <div className="grid md:grid-cols-2">
+          <img
+            src="img/cezerin-mobile-order-summary.png"
+            alt="Cezerin Mobile Order Summary"
+            title="Cezerin Mobile Order Summary"
+            className="justify-self-center"
+          />
+          <div className="place-self-center">
+            <h2>We Are Building A Better Future</h2>
+            <p>
+              We use latest technologies to build web apps with efficient use of
+              the network. Web app loads once on startup and fetches a smaller
+              amount of data when you request a new page.
+            </p>
+            <h2>Progressive Web Apps</h2>
+            <p>
+              Building a high-quality Progressive Web App has incredible
+              benefits, making it easy to delight your users, grow engagement
+              and increase conversions.
+            </p>
+            <p>
+              <a href="https://plusha.cezerin.net">Demo Store - Plusha Theme</a>
+            </p>
+            <p>
+              <a href="https://cezerin.net">Demo Store - Default Theme</a>
+            </p>
+            <p>
+              <a href="https://cezerin.net/admin">Demo Store - Dashboard</a>
+            </p>
+          </div>
         </div>
-      </div>
-    </header>
-  )
-}
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+        <h2>Features</h2>
+        <ul className="grid md:grid-cols-2">
+          <li>Products (options, variants, attributes)</li>
+          <li>Product categories</li>
+          <li>Inventory and stock management</li>
+          <li>Customers</li>
+          <li>Customer groups</li>
+          <li>Orders</li>
+          <li>Order custom statuses</li>
+          <li>Pages</li>
+          <li>File manager</li>
+          <li>Shipping and payment methods</li>
+          <li>Custom shipping fields</li>
+          <li>Payment Gateways</li>
+          <li>Checkout options</li>
+          <li>Access Tokens</li>
+          <li>Export theme to zip archive</li>
+          <li>Install theme from zip archive</li>
+          <li>Theme Settings</li>
+          <li>Apps Store</li>
+          <li>Server-side rendering (SEO friendly)</li>
+          <li>Passwordless</li>
+        </ul>
+
+        <h2>Plusha Theme</h2>
+        <img
+          src="img/cezerin-plusha-theme.png"
+          alt="Cezerin Plusha Theme"
+          title="Cezerin Plusha Theme"
+          className="mx-auto block md:w-2/3 lg:w-1/2"
+        />
+        <h2>Default Theme</h2>
+        <img
+          src="img/cezerin-default-theme.png"
+          alt="Cezerin Default Theme"
+          title="Cezerin Default Theme"
+          className="mx-auto block md:w-2/3 lg:w-1/2"
+        />
+        <h2>Dashboard</h2>
+        <img
+          src="img/cezerin-dashboard-products.png"
+          alt="Cezerin Dashboard"
+          title="Cezerin Dashboard"
+          className="mx-auto block md:w-2/3 lg:w-1/2"
+        />
+
+        <h2>Roadmap</h2>
+        <p>
+          The future roadmap of Cezerin includes a number of focus areas, some
+          of which are detailed below (in no particular order)
+        </p>
+
+        <ul className="grid md:grid-cols-2">
+          <li>Documentation</li>
+          <li>Automated tests</li>
+          <li>Central update</li>
+          <li>Themes Store</li>
+          <li>WebHooks</li>
+          <li>Realtime API</li>
+          <li>Shipping Integrations</li>
+          <li>Customer Account</li>
+          <li>Push Notifications</li>
+          <li>Braintree Payments</li>
+          <li>Stripe Payments</li>
+        </ul>
+
+        <h2>Join Us</h2>
+        <LinkElement />
       </main>
-    </Layout>
-  )
-}
+    </div>
+  </Layout>
+)
+
+export default Home
