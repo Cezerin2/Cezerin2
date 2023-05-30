2acca7810f85da29a1f31690c605b022d80f52d8
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2023-05-30 07:18:18 -0700

    feat(docs): Adding stripe payments (#1792)

--- src/docs/docusaurus.config.js
+++ src/docs/docusaurus.config.js
@@ -65,10 +65,10 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "Home",
         logo: {
-          alt: "My Site Logo",
           src: "img/logo.svg",
+          alt: "Site Logo",
         },
         items: [
           {
