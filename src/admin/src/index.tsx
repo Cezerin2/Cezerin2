6843e2c3bdcbb7d1990d9ed7799bf5088b859192
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2023-06-03 10:10:11 +0530

    chore(admin): Moving admin files (#1838)
    * feat(admin): Adding settings form
    
    * chore(admin): Moving admin files
    
    * feat(admin): Updating admin

--- src/admin/src/index.tsx
+++ src/admin/src/index.tsx
@@ -1,15 +1,36 @@
-import React from "react"
+import { configureStore } from "@reduxjs/toolkit"
+import { connectToWebSocket } from "lib/apiWebSocket"
+import * as auth from "lib/auth"
+import settings from "lib/settings"
+import { fetchSettings } from "modules/settings/actions"
+import React, { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import reportWebVitals from "./reportWebVitals"
+import { Provider } from "react-redux"
+import App from "./app"
+import "./css/flexboxgrid.min.css"
+import "./css/style.css"
+import "./i18n"
+// import reportWebVitals from "./reportWebVitals"
+import reducer from "./rootReducer"
+
+if (!settings.developerMode) auth.validateCurrentToken()
+
+const store = configureStore({ reducer })
+store.dispatch(fetchSettings())
+
+if (window.WebSocket) connectToWebSocket(store)
+else console.log("WebSocket is not supported by your browser.")
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <div />
-  </React.StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
---
+++ src/admin/src/settingsForm.tsx
@@ -0,0 +1,71 @@
+import { Button, MenuItem } from "@mui/material"
+import { forOwn } from "lodash"
+import { Select, Switches, TextField } from "mui-rff"
+import { Form } from "react-final-form"
+
+export const SettingsForm = () => (
+  <Form
+    onSubmit={values => {
+      forOwn(values, (value, key) => localStorage.setItem(key, value))
+      window.location.reload()
+    }}
+    initialValues={{
+      language: "en",
+      apiBaseUrl: "http://localhost:3001/api/v1",
+      apiWebSocketUrl: "ws://localhost:3001",
+      developerMode: true,
+    }}
+  >
+    {({ handleSubmit, form: { reset }, submitting, pristine, values }) => (
+      <form onSubmit={handleSubmit}>
+        <Select
+          name="language"
+          label="Language"
+          formControlProps={{ margin: "normal" }}
+        >
+          <MenuItem value="de">de</MenuItem>
+          <MenuItem value="en_US">en_US</MenuItem>
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="fr">fr</MenuItem>
+          <MenuItem value="it">it</MenuItem>
+          <MenuItem value="pt_BR">pt_BR</MenuItem>
+          <MenuItem value="ru">ru</MenuItem>
+          <MenuItem value="si">si</MenuItem>
+          <MenuItem value="ta">ta</MenuItem>
+          <MenuItem value="uk">uk</MenuItem>
+          <MenuItem value="zh_CN">zh_CN</MenuItem>
+        </Select>
+        <TextField
+          name="apiBaseUrl"
+          label="API Base URL"
+          placeholder="http://yourDomain/api/v1"
+          required
+        />
+        <TextField
+          name="apiWebSocketUrl"
+          label="API WebSocket URL"
+          placeholder="ws://yourDomain"
+          required
+        />
+        <Switches
+          name="developerMode"
+          required
+          data={{ label: "Developer Mode", value: null }}
+        />
+        <div>
+          <Button type="submit" disabled={submitting}>
+            Connect
+          </Button>
+          <Button
+            type="button"
+            onClick={reset}
+            disabled={submitting || pristine}
+          >
+            Reset
+          </Button>
+        </div>
+        <pre>{JSON.stringify(values, null, 2)}</pre>
+      </form>
+    )}
+  </Form>
+)
