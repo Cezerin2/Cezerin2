a5221b45b0d518874880fe929853d49b5e7c65a3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2023-06-18 09:10:22 +0530

    feat(server): Adding stripe payment intents (#1917)
    * feat(server): Updating stripe
    
    * feat(server): Adding stripe payment intents

--- src/server/paymentGateways/StripeElements.ts
+++ src/server/paymentGateways/StripeElements.ts
@@ -1,6 +1,6 @@
-import stripePackage from "stripe"
-import OrdersService from "../services/orders/orders"
+import Stripe from "stripe"
 import OrdertTansactionsService from "../services/orders/orderTransactions"
+import OrdersService from "../services/orders/orders"
 
 const getPaymentFormSettings = options => {
   const { gateway, gatewaySettings, order, amount, currency } = options
@@ -15,44 +15,40 @@ const getPaymentFormSettings = options => {
 }
 
 const processOrderPayment = async ({ order, gatewaySettings, settings }) => {
-  try {
-    const stripe = stripePackage(gatewaySettings.secret_key)
-    const charge = await stripe.charges.create({
-      amount: order.grand_total * 100,
-      currency: settings.currency_code,
-      description: `Order #${order.number}`,
-      statement_descriptor: `Order #${order.number}`,
-      metadata: {
-        order_id: order.id,
-      },
-      source: order.payment_token,
+  const stripe = new Stripe(gatewaySettings.secret_key, {
+    apiVersion: "2022-11-15",
+  })
+
+  const intent = await stripe.paymentIntents.create({
+    amount: order.grand_total * 100,
+    currency: settings.currency_code,
+    automatic_payment_methods: { enabled: true },
+    confirm: true,
+    description: `Order #${order.number}`,
+    metadata: { order_id: order.id },
+    payment_method: order.payment_token,
+    return_url: `${settings.domain}/checkout`,
+    statement_descriptor: `Order #${order.number}`,
+  })
+
+  const paymentSucceeded = intent.status === "succeeded"
+
+  if (paymentSucceeded)
+    await OrdersService.updateOrder(order.id, {
+      paid: true,
+      date_paid: new Date(),
     })
 
-    // status: succeeded, pending, failed
-    const paymentSucceeded =
-      charge.status === "succeeded" || charge.paid === true
+  await OrdertTansactionsService.addTransaction(order.id, {
+    transaction_id: intent.id,
+    amount: intent.amount / 100,
+    currency: intent.currency,
+    status: intent.status,
+    details: intent.status, // TODO: Unnecessary
+    success: paymentSucceeded,
+  })
 
-    if (paymentSucceeded) {
-      await OrdersService.updateOrder(order.id, {
-        paid: true,
-        date_paid: new Date(),
-      })
-    }
-
-    await OrdertTansactionsService.addTransaction(order.id, {
-      transaction_id: charge.id,
-      amount: charge.amount / 100,
-      currency: charge.currency,
-      status: charge.status,
-      details: charge.outcome.seller_message,
-      success: paymentSucceeded,
-    })
-
-    return paymentSucceeded
-  } catch (error) {
-    // handle errors
-    return false
-  }
+  return paymentSucceeded
 }
 
 export default {
