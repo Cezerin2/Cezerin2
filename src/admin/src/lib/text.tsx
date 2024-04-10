aa82c34197dd069f10fb1bac700e8e4d4491a343
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-04-09 23:46:29 -0700

    feat(admin): Moving to craco

--- src/admin/src/lib/text.tsx
+++ src/admin/src/lib/text.tsx
@@ -1,4 +1,3 @@
-import text from "../../../../public/admin-assets/locales/en_US.json"
+import applicationText from "./en_US.json"
 
-// Will be replaced by webpack
-export default applicationText as typeof text
+export default applicationText
